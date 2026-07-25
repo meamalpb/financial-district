5595ab79bb4f95a6f58f2eb09aec470b9abed494
Amal PB <321amalpb.com@gmail.com>
2026-07-25 20:48:51 +0530

    add caller service and demonstrate service discovery

---
+++ caller-service/src/main/java/caller_service/CallerServiceApplication.java
@@ -0,0 +1,13 @@
+package caller_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallerServiceApplication.class, args);
+	}
+
+}
